a8f240cf84b761f30bed09fa6ebf5717e7fff4c6
tobiaditia <tobiaditia549@gmail.com>
2022-02-06 12:12:08 +0700

    penjelasan

--- kriptografi/script.js
+++ kriptografi/script.js
@@ -1,29 +1,25 @@
 $(document).ready(function(){
+    let alphabetToArray = 'abcdefghijklmnopqrstuvwxyz'.split(''); //merubah alphabet ke array
     
     $('#generate').on('click', function () {
-        
-        let alphabetToArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
         var keyText = $('#kunci').val();
         var plaintext = $('#plaintext').val();
-        
-        // var withoutSpacePlainText = plaintext.replace(/ /g, '');
-        // var withoutSpacePlainTextToArray = withoutSpacePlainText.split('');
 
-        var withoutSpacePlainTextToArray = plaintext.split('');
+        var plainTextToArray = plaintext.split(''); //merubah plain text ke array
 
-        var keyTextToArray = keyText.split('');
+        var keyTextToArray = keyText.split(''); //merubah key/kunci ke array
 
         var keyIndexNow = 0;
-        var keyTextGenerate = '';
+        var keyTextGenerate = ''; //untuk menyimpan kunci yang sama panjang dengan plaintext
 
         // proses generate key text menjadi sepanjang plaintext
-        for (let index = 0; index < withoutSpacePlainTextToArray.length; index++) {
-            if (withoutSpacePlainTextToArray[index] == ' ') { //jika ada spasi di plaintext
+        for (let index = 0; index < plainTextToArray.length; index++) {
+            if (plainTextToArray[index] == ' ') { //jika ada spasi di plaintext
                 keyTextGenerate += ' ';
             } else {
-                keyTextGenerate += keyTextToArray[keyIndexNow];
-                if (keyIndexNow == keyTextToArray.length - 1) {
+                keyTextGenerate += keyTextToArray[keyIndexNow]; //menambah huruf ke keyTextGenerate dengan urutannya
+                if (keyIndexNow == keyTextToArray.length - 1) { //apabila huruf di key sudah habis maka mengulang dari awal
                     keyIndexNow = 0;
                 } else {
                     keyIndexNow++;
@@ -31,22 +27,17 @@ $(document).ready(function(){
             }
         }
 
-        var keyTextGenerateToArray = keyTextGenerate.split('');
-        var chiperText = '';
+        var keyTextGenerateToArray = keyTextGenerate.split(''); //merubah hasil generate key ke array
+        var chiperText = ''; //untuk menyimpan hasil/chipertext
 
-        for (let index = 0; index < withoutSpacePlainTextToArray.length; index++) {
-            if (withoutSpacePlainTextToArray[index] == ' ') { //jika ada spasi di plaintext
+        for (let index = 0; index < plainTextToArray.length; index++) {
+            if (plainTextToArray[index] == ' ') { //jika ada spasi di plaintext
                 chiperText += ' ';
             } else {
-                keyTextGenerate += keyTextToArray[keyIndexNow];
-                if (keyIndexNow == keyTextToArray.length - 1) {
-                    keyIndexNow = 0;
-                } else {
-                    var indexPlainText = alphabetToArray.indexOf(withoutSpacePlainTextToArray[index]);
-                    var indexKeyText = alphabetToArray.indexOf(keyTextGenerateToArray[index]);
+                var indexPlainText = alphabetToArray.indexOf(plainTextToArray[index]); //mencari no index dari setiap plaintext
+                var indexKeyText = alphabetToArray.indexOf(keyTextGenerateToArray[index]); //mencari no index dari setiap keytext
 
-                    chiperText += alphabetToArray[(indexPlainText + indexKeyText) % alphabetToArray.length];
-                }
+                chiperText += alphabetToArray[(indexPlainText + indexKeyText) % alphabetToArray.length]; // proses huruf untuk chipertext 
             }
             
         }
