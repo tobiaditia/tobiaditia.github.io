1eeb71258e7d7a366509b539e47859d0463935d9
tobiaditia <tobiaditia549@gmail.com>
2022-02-07 08:20:23 +0700

    kriptografi: feature decrypt

--- kriptografi/script.js
+++ kriptografi/script.js
@@ -27,17 +27,20 @@ $(document).ready(function(){
             }
         }
 
+        
         var keyTextGenerateToArray = keyTextGenerate.split(''); //merubah hasil generate key ke array
         var chiperText = ''; //untuk menyimpan hasil/chipertext
-
+        
         for (let index = 0; index < plainTextToArray.length; index++) {
             if (plainTextToArray[index] == ' ') { //jika ada spasi di plaintext
                 chiperText += ' ';
             } else {
-                var indexPlainText = alphabetToArray.indexOf(plainTextToArray[index]); //mencari no index dari setiap plaintext
-                var indexKeyText = alphabetToArray.indexOf(keyTextGenerateToArray[index]); //mencari no index dari setiap keytext
+                var indexPlainText = alphabetToArray.indexOf(plainTextToArray[index].toLowerCase()); //mencari no index dari setiap plaintext , mengubah semua ke huruf kecil
+                var indexKeyText = alphabetToArray.indexOf(keyTextGenerateToArray[index].toLowerCase()); //mencari no index dari setiap keytext , mengubah semua ke huruf kecil
+                
+                var indexProccess = (indexPlainText + indexKeyText) % alphabetToArray.length; //proses mencari index untuk plaintext dengan rumus
+                chiperText += alphabetToArray[indexProccess]; // proses huruf untuk chipertext 
 
-                chiperText += alphabetToArray[(indexPlainText + indexKeyText) % alphabetToArray.length]; // proses huruf untuk chipertext 
             }
             
         }
@@ -50,5 +53,52 @@ $(document).ready(function(){
         $('#plaintext').val('');
         $('#kunci').val('');
         $('#chipertext').val('');
+    });
+
+    $('#generateInDec').on('click', function () {
+
+        var keyText = $('#kunciInDec').val();
+        var chipertext = $('#chipertextInDec').val();
+
+        var chiperTextToArray = chipertext.split(''); //merubah plain text ke array
+
+        var keyTextToArray = keyText.split(''); //merubah key/kunci ke array
+
+        var keyIndexNow = 0;
+        var keyTextGenerate = ''; //untuk menyimpan kunci yang sama panjang dengan chipertext
+
+        // proses generate key text menjadi sepanjang chipertext
+        for (let index = 0; index < chiperTextToArray.length; index++) {
+            if (chiperTextToArray[index] == ' ') { //jika ada spasi di chipertext
+                keyTextGenerate += ' ';
+            } else {
+                keyTextGenerate += keyTextToArray[keyIndexNow]; //menambah huruf ke keyTextGenerate dengan urutannya
+                if (keyIndexNow == keyTextToArray.length - 1) { //apabila huruf di key sudah habis maka mengulang dari awal
+                    keyIndexNow = 0;
+                } else {
+                    keyIndexNow++;
+                }
+            }
+        }
+
+        var keyTextGenerateToArray = keyTextGenerate.split(''); //merubah hasil generate key ke array
+        var plainText = ''; //untuk menyimpan hasil/plainText
+
+        for (let index = 0; index < chiperTextToArray.length; index++) {
+            if (chiperTextToArray[index] == ' ') { //jika ada spasi di chipertext
+                plainText += ' ';
+            } else {
+                var indexChiperText = alphabetToArray.indexOf(chiperTextToArray[index].toLowerCase()); //mencari no index dari setiap chipertext
+                var indexKeyText = alphabetToArray.indexOf(keyTextGenerateToArray[index].toLowerCase()); //mencari no index dari setiap keytext
+
+                var indexProccess = (indexChiperText - indexKeyText) % alphabetToArray.length; //proses mencari index untuk plaintext dengan rumus
+                plainText += alphabetToArray[(indexProccess < 0) ? indexProccess + 26 : indexProccess]; // proses mencari huruf berdasarkan index
+
+            }
+
+        }
+
+        $('#plaintextInDec').val(plainText);
+
     });
 });
